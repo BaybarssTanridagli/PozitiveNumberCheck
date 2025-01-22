6efe43c257a4398f19e4cfe3994e436e8de5bdb4
Baybars <baybarsstanridagli@gmail.com>
2025-01-22 15:52:09 +0300

    PositiveNumberCheck

---
+++ PozitiveNumberCheck/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PozitiveNumberCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Please Enter The Number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number >= 0)
+            {
+                Console.WriteLine("This Number is Positive");
+            }
+            else
+            {
+                Console.WriteLine("This Number is Negative");
+            }
+            Console.ReadKey();
+        }
+    }
+}
